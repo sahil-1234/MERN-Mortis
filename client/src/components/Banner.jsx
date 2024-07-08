a69199a1e3cfc6cbd598579717b554c80f64c08c
SAHIL SRIVASTAV <96813045+sahil-1234@users.noreply.github.com>
2024-07-08 23:01:32 +0530

    Update Banner.jsx

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -15,7 +15,7 @@ const Banner = () => {
   return (
     <section
       className="relative"
-      style={bannerStyle} // Apply the style here
+      style={bannerStyle} 
     >
     <div
     className="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
