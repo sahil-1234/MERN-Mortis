f3cc272bc1b270efac53f9773dda9d4b3b9c4b64
SAHIL SRIVASTAV <96813045+sahil-1234@users.noreply.github.com>
2024-07-19 22:22:20 +0530

    Update userSchema.js

--- models/userSchema.js
+++ models/userSchema.js
@@ -43,7 +43,7 @@ const userSchema=new mongoose.Schema({
     role:{
         type:String,
         required:true,
-        enum:["Admin","Patient","Doctor"]  // doctor for making future pages of doctor
+        enum:["Admin","Patient","Doctor"]  
     },
     doctorDepartment:{
     type:String
